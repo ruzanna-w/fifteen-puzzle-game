6ffa80bc02d892391d0af9445540631548cdabf2
Ruzanna <ruzanna30a@gmail.com>
2025-05-29 01:09:33 +0400

    Fix board size input handling

--- launch_game.py
+++ launch_game.py
@@ -5,17 +5,21 @@ print('''\033[35mДобро пожаловать в игру пятнашки!
 Вам необходимо ввести размер поля, например, 4 для поля 4x4
 при этом размер поля должет быть больше 2 \033[0m''')
 
-n = int(input('\nВведите размер поля: '))
+
 
 while True:
-    if n <= 2:
-        print('Размер поля должен быть не меньше 3х3')
+    try:
         n = int(input('\nВведите размер поля: '))
-    else:
-        clear()
-        print(f'Вы выбрали размер {n}x{n}\n')
-        numbers = generate_numb(n)
-        solvability_check = find_inversions(n, numbers)
-        matrix, empty_position_row, empty_position_column = generate_and_print_field(n, numbers, solvability_check)
-        move_digits(matrix, empty_position_row, empty_position_column)
-        break
+        if n <= 2:
+            print('Размер поля должен быть не меньше 3х3')
+            continue
+        else:
+            clear()
+            print(f'Вы выбрали размер {n}x{n}\n')
+            numbers = generate_numb(n)
+            solvability_check = find_inversions(n, numbers)
+            matrix, empty_cell_row, empty_cell_column = generate_and_print_field(n, numbers, solvability_check)
+            move_digits(matrix, empty_cell_row, empty_cell_column)
+            break
+    except ValueError:
+        print('Введите корректное значение для поля (например, 4х4)')
